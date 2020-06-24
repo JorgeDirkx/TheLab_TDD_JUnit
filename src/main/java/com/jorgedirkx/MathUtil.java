d3f8139934d5cc570c7d2bb0e50cbc58e68c7663
JorgeDirkx <jorge.dirkx@gmail.com>
2020-06-24 14:04:17 +0200

    adding tests

--- src/main/java/com/jorgedirkx/MathUtil.java
+++ src/main/java/com/jorgedirkx/MathUtil.java
@@ -5,4 +5,23 @@ public class MathUtil {
     public static int add(int num1, int num2){
         return num1+num2;
     }
+
+    public static int multiple(int num1, int num2){
+        return num1*num2;
+    }
+
+    public static boolean isEven(int num){
+        if(num%2==0){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isPrime(int num){
+        for(int i =2; 2*i<num;i++){
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
 }
--- src/test/java/com/jorgedirkx/MathUtilTest.java
+++ src/test/java/com/jorgedirkx/MathUtilTest.java
@@ -4,6 +4,8 @@ import com.jorgedirkx.MathUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MathUtilTest {
 
     MathUtil mathUtil = new MathUtil();
@@ -11,5 +13,20 @@ public class MathUtilTest {
     @Test
     void test_Add(){
         Assertions.assertEquals(5, MathUtil.add(3,2));
+    }
+
+    @Test
+    void test_Mult(){
+        Assertions.assertEquals(6, MathUtil.multiple(3,2));
+    }
+
+    @Test
+    void test_Even(){
+        MathUtil.isEven(5);
+    }
+
+    @Test
+    void testIs_Prime(){
+        assertTrue(MathUtil.isPrime(13));
     }
 }
